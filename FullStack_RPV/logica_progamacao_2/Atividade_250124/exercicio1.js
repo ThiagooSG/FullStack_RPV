76ddbf86d6b694bead3785ae45076ae4b05e7624
ThiagooSG <thiagosg.ti@gmail.com>
2024-01-28 13:13:43 -0300

    Código Exercicio 1 dia 25/01/24 refatorado

--- FullStack_RPV/logica_progamacao_2/Atividade_250124/exercicio1.js
+++ FullStack_RPV/logica_progamacao_2/Atividade_250124/exercicio1.js
@@ -1,18 +1,5 @@
-console.clear();
 const promptSync = require('prompt-sync');
 const prompt = promptSync();
-
-console.log("Bem Vindo!");
-let nomeHospede = prompt("Digite seu nome para inicializar: ")
-console.clear();
-
-console.log("inicializando.");
-setTimeout(function () {
-    console.log("inicializando..");
-}, 1000);
-setTimeout(function () {
-    console.log("inicializando...");
-}, 1200);
 
 const menu = `=================================
 -PARADISE HOTEL - MENU DE OPÇÕES-
@@ -23,31 +10,52 @@ const menu = `=================================
   4 - Fazer Check-out
   5 - Sair
 =================================`;
-setTimeout(function () {
+
+function inicializar() {
     console.clear();
-    console.log(`Olá ${nomeHospede}, escolha uma das opções no menu: `);
-    console.log(menu);
-    let opcao = prompt(`Escolha uma opção: `);
-    while(opcao) {
-        if(opcao == 1) {
+    console.log("Bem Vindo!");
+    let nomeHospede = prompt("Digite seu nome para inicializar: ");
+    console.clear();
+    console.log("inicializando.");
+    setTimeout(() => console.log("inicializando.."), 1000);
+    setTimeout(() => console.log("inicializando..."), 1200);
+    return nomeHospede;
+}
+
+function exibirMenu(nomeHospede) {
+    setTimeout(() => {
+        console.clear();
+        console.log(`Olá ${nomeHospede}, escolha uma das opções no menu: `);
+        console.log(menu);
+        let opcao = prompt(`Escolha uma opção: `);
+        processarOpcao(opcao, nomeHospede);
+    }, 1700);
+}
+
+function processarOpcao(opcao, nomeHospede) {
+    switch(opcao) {
+        case '1':
             console.log(`${nomeHospede}, seu Check-in foi realizado com sucesso.`);
             break;
-        } else if(opcao == 2) {
+        case '2':
             console.log(`${nomeHospede}, serviço de quarto selecionado, em breve o Sr(a) será atendido.`);
-            break;            
-        } else if(opcao == 3) {
+            break;
+        case '3':
             console.log(`${nomeHospede}, pedido selecionado, em breve o Sr(a) receberá o menu.`);
-            break; 
-        } else if(opcao == 4) {
+            break;
+        case '4':
             console.log(`${nomeHospede}, Check-out realizado com sucesso`);
             console.log(`Obrigado por usar nossos serviços!`);
-            break; 
-        } else if(opcao == 5) {
+            break;
+        case '5':
             console.log(`Finalizando menu de opções!`);
-            break
-        } else {
-            console.log(`Opção inválida, escolha um número de 1 a 5 no menu.`)
+            break;
+        default:
+            console.log(`Opção inválida, escolha um número de 1 a 5 no menu.`);
             opcao = prompt(`Escolha uma opção: `);
-        }
+            processarOpcao(opcao, nomeHospede);
     }
-}, 1700);
+}
+
+let nomeHospede = inicializar();
+exibirMenu(nomeHospede);
